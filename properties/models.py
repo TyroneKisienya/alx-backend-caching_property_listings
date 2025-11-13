e760683bcdcb681f6ca5e8cda3b269f1163bef96
Skai <tyronekisienya01@gmail.com>
2025-11-13 17:28:59 +0300

    naming

--- properties/models.py
+++ properties/models.py
@@ -7,4 +7,4 @@ class Property(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.CharField(max_length=100)
-    creatd_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
